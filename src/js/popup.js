9a115289964311319b01da2c0a4dd9a8e7132742
ricozhuthegreat <33135503+ricozhuthegreat@users.noreply.github.com>
2019-03-11 18:43:41 -0400

    Added Search Query Text Nodes
     - Added a list of H1 Search Query text nodes in popup.js
     - Changed placeholder text in popup.html
     - TODO: Make Search query characters in array instead of single H1 and use a custom class with css to draw

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,4 +1,36 @@
 
+// Search Query Text Nodes (H1)
+
+// Text node for the A search field query (Uppercase)
+const aQueryUp = "À Â";
+// Text node for the a search field query (Lowercase)
+const aQueryLow = "à â";
+
+// Text node for the E search field query (Uppercase)
+const eQueryUp = "È É Ê Ë";
+// Text node for the e search field query (Lowercase)
+const eQueryLow = "è é ê ë";
+
+// Text node for the I search field query (Uppercase)
+const iQueryUp = "Î Ï";
+// Text node for the i search field query (Lowercase)
+const iQueryLow = "î ï";
+
+// Text node for the O search field query (Uppercase)
+const oQueryUp = "Ô Œ";
+// Text node for the o search field query (Lowercase)
+const oQueryLow = "ô œ";
+
+// Text node for the U search field query (Uppercase)
+const uQueryUp = "Ù Û Ü";
+// Text node for the u search field query (Lowercase)
+const uQueryLow = "ù û ü";
+
+// Text node for the Y search field query (Uppercase)
+const yQueryUp = "Ÿ";
+// Text node for the y search field query (Lowercase)
+const yQueryLow = "ÿ";
+
 
 function displaySearchQuery () {
 
@@ -18,5 +50,5 @@ function clearRoot () {
   while (rootNode.firstChild) {
     rootNode.removeChild(rootNode.firstChild);
   }
-  
+
 }
